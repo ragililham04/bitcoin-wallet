9f01bafd4f82632a60f2eb8cfeb7e9b12559910e
ragililham04 <ragililham04@gmail.com>
2026-09-04 23:04:35 +0000

    Initial commit: tests/Engine.Tests/NetworkRegistryTests.cs

---
+++ tests/Engine.Tests/NetworkRegistryTests.cs
@@ -0,0 +1,24 @@
+using BtcWallet.Engine.Networks;
+using Xunit;
+
+namespace BtcWallet.Engine.Tests;
+
+public class NetworkRegistryTests
+{
+    [Fact]
+    public void GetNetwork_ReturnsEthereumDescriptor()
+    {
+        var registry = new NetworkRegistry();
+        var network = registry.GetNetwork("ethereum-mainnet");
+
+        Assert.Equal("ETH", network.NativeAssetSymbol);
+        Assert.Equal("evm", network.ChainType);
+    }
+
+    [Fact]
+    public void GetAllNetworks_ReturnsAtLeastFourNetworks()
+    {
+        var registry = new NetworkRegistry();
+        Assert.True(registry.GetAllNetworks().Count >= 4);
+    }
+}
